1c5f4c03734b17de6f10ae44caaffcd002e667cc
Loa212 <anobileloris@gmail.com>
2022-12-10 12:05:37 +0100

    useStrapiCms

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind()]
 });
---
+++ src/components/react/BlogGrid.tsx
@@ -0,0 +1,35 @@
+import getApiUrl from "../../utils/getApiUrl";
+
+const BlogGrid = ({ posts }: any) => {
+  return (
+    <ul className="grid grid-cols-2 gap-6">
+      {posts.map((post: any, idx: number) => (
+        <li key={idx} className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              className="object-contain w-full h-48"
+              src={
+                getApiUrl() +
+                `${post.attributes.featuredImage.data.attributes.url}`
+              }
+              alt={post.attributes.featuredImage.data.attributes.name}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{post.attributes.title}</h2>
+            <p>{post.attributes.excerpt}</p>
+
+            <div className="card-actions flex items-center justify-between">
+              <div className="badge badge-outline">
+                {post.attributes.readingTime}
+              </div>
+              <button className="btn btn-primary">Leggi articolo</button>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BlogGrid;
