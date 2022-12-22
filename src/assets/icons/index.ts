4f669f2f4a892a1d7376ce555de30ba41de16570
Loa212 <anobileloris@gmail.com>
2022-12-22 17:49:21 +0100

    what I Do

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,7 @@
+// @ts-nocheck
 import Close from "./Close.astro";
+import MdStayPrimaryPortrait from "./MdStayPrimaryPortrait.astro";
+import MdArtTrack from "./MdArtTrack.astro";
+import MdWeb from "./MdWeb.astro";
 
-export { Close };
+export { Close, MdStayPrimaryPortrait, MdArtTrack, MdWeb };
